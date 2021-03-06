3551c99e194e48af3f64fa5129e67738db224d19
xcript1 <o_olatunji@mail.harper.edu>
2021-03-06 02:52:16 -0600

    New Images added

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,16 @@ const showMenu = (toggleId, navId) => {
 
 showMenu('nav-toggle','nav-menu')
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    //Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
